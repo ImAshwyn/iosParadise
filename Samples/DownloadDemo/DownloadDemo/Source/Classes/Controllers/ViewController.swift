9a0e64fddb6a3f2b514c33cd81383ebf52fa2551
Ashwin <ashwin.kini@robosoftin.com>
2016-03-16 11:48:12 +0530

    Adding download sample project

---
+++ Samples/DownloadDemo/DownloadDemo/Source/Classes/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DownloadDemo
+//
+//  Created by Ashwin Kini Ullal on 15/03/16.
+//  Copyright (c) 2016 Robosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Samples/DownloadDemo/DownloadDemo/Source/Classes/OtherUtilities/DownloadManager.swift
@@ -0,0 +1,11 @@
+//
+//  DownloadManager.swift
+//  Created by Ashwin Kini  on 15/03/16.
+
+import UIKit
+
+class DownloadManager: NSObject {
+
+    static let sharedManager = DownloadManager()
+    
+}
