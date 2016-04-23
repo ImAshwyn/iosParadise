959df7dd4bf7bb313b200b91afd988b03127d11a
ImAshwyn <ashwin.kini@icloud.com>
2016-04-23 12:54:55 +0530

    Initial commit

---
+++ Samples/Budwisher/Budwisher/Budwisher/Source/Classes/OtherUtilities/DataManager.swift
@@ -0,0 +1,13 @@
+//  DataManager.swift
+//  Budwisher
+//
+//  Created by Ashwin Kini on 23/04/16.
+//  Copyright © 2016 iPark. All rights reserved.
+//
+import UIKit
+
+class DownloadManager: NSObject {
+
+    static let sharedManager = DownloadManager()
+    
+}
